064341e8bfd696a2a2e70d22d41d309d8cabd074
Trey <ttangeman97@gmail.com>
2017-07-11 14:18:02 -0700

    Add files

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcc -std=c99 rle.c -o rle
---
+++ rle.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+void compress(char *infn, char *outfn)
+{
+    uint8_t *memory;
+    size_t size;
+    FILE *inf = fopen(infn, "r");
+    FILE *outf = fopen(outfn, "w");
+
+    if (!inf) {
+        fprintf(stderr, "Unable to open input file!\n");
+        exit(2);
+    } else if (!outf) {
+        fprintf(stderr, "Unable to open output file!\n");
+        exit(2);
+    }
+
+    fseek(inf, 0, SEEK_END);
+    size = ftell(inf);
+    rewind(inf);
+
+    memory = malloc(size);
+
+    fread(memory, 1, size, inf);
+    fclose(inf);
+
+    for (int i = 0; i < size; ) {
+        uint8_t count = 1;
+        uint8_t byte = memory[i];
+        while (memory[++i] == byte)
+            count++;
+        fputc(count, outf);
+        fputc(byte, outf);
+    }
+
+    fclose(outf);
+    free(memory);
+}
+
+void decompress(char *infn, char *outfn)
+{
+    uint8_t *memory;
+    size_t size;
+    FILE *inf = fopen(infn, "r");
+    FILE *outf = fopen(outfn, "w");
+
+    if (!inf) {
+        fprintf(stderr, "Unable to open input file!\n");
+        exit(2);
+    } else if (!outf) {
+        fprintf(stderr, "Unable to open output file!\n");
+        exit(2);
+    }
+
+    fseek(inf, 0, SEEK_END);
+    size = ftell(inf);
+    rewind(inf);
+
+    memory = malloc(size);
+
+    fread(memory, 1, size, inf);
+    fclose(inf);
+
+    for (int i = 0, j = 1; i < size && j < size; i += 2, j += 2) {
+        uint8_t count = memory[i];
+        uint8_t byte = memory[j];
+
+        for (int k = 0; k < count; k++)
+            fputc(byte, outf);
+    }
+
+    fclose(outf);
+    free(memory);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 4) {
+        printf("Usage: %s <OPTION> <INPUT> <OUTPUT>\n", argv[0]);
+        printf("Options:\n");
+        printf("\tcompress\n");
+        printf("\tdecompress\n");
+        exit(1);
+    }
+
+    if (strcmp(argv[1], "compress") == 0) {
+        char *infn = argv[2];
+        char *outfn = argv[3];
+        compress(infn, outfn);
+    } else if (strcmp(argv[1], "decompress") == 0) {
+        char *infn = argv[2];
+        char *outfn = argv[3];
+        decompress(infn, outfn);
+    } else {
+        fprintf(stderr, "No valid option detected.\n");
+        exit(1);
+    }
+}
